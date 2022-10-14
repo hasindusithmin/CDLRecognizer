5aa1bf0e250cf3ab85c75c0352a1519c850f45e9
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-14 19:43:47 +0530

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="CDLRecognizeAPI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    raise HTTPException(status_code=200)
