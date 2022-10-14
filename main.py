fe7957edf6413f787cd83bafac109d3e2e519c86
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-14 20:49:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ import numpy as np
 from fastapi import FastAPI,HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
-
-
 
 app = FastAPI(title="CDLRecognizeAPI")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.mount('/static',StaticFiles(directory='static'),name='static')
 
